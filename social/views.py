1eaf7ca80461c9ba52fcdf8f3e2720e8c00786b1
Veries Seals <veries@me.com>
2025-09-08 17:59:47 -0500

    Put imports in social/views.py

--- social/views.py
+++ social/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# 5) Views (feed, signup, post, react, follow, share, profile)from django.contrib import messages
+
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.db.models import Q
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
+from .forms import SignUpForm, PostForm, ReplyForm, ProfileForm
+from .models import Post, Reaction, Share, Profile
 
 # Create your views here.
+# -----------------------------------------------
+
+def signup_view(request):
+    pass
