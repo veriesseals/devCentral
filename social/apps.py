ed243310ac6061eb03ccaac0014b55b3963f03cb
Veries Seals <veries@me.com>
2025-09-08 15:46:21 -0500

    Added a method to import signals to ensure they are registered

--- social/apps.py
+++ social/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class SocialConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'social'
+
+    def ready(self):
+        from . import signals # Import signals to ensure they are registered
