44d4f41d48d47906c9c99762d9eb56cbc041caae
Veries Seals <veries@me.com>
2025-09-08 15:55:10 -0500

    Created signals.py in social/signals.py

---
+++ social/signals.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Profile
+
+# Create or update user profile on user creation
+# -----------------------------------------------
+@receiver(post_save, sender=User)
+def make_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
+    
+    
